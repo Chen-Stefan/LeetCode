2f670034b928c793d94f78cb09b2afd61ba8c524
Chen-Stefan <91659971+Chen-Stefan@users.noreply.github.com>
2022-09-10 16:57:40 -0700

    Time: 65 ms (23.20%), Space: 14.1 MB (36.11%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -8,16 +8,11 @@ class Solution:
         self.dfs(nums, 0, subset, results)
         return results
         
-    def dfs(self, nums, index, subset, results):
-        # 递归的出口
-        if index == len(nums):
-            results.append(copy.deepcopy(subset))
-            return 
-        # 递归的拆解
-        # 选nums[index]
-        subset.append(nums[index])
-        self.dfs(nums, index + 1, subset, results)
+    def dfs(self, nums, startIndex, subset, results):
+        results.append(copy.deepcopy(subset))
+        for index in range(startIndex, len(nums)):
+            subset.append(nums[index])
+            self.dfs(nums, index + 1, subset, results)
+            subset.pop(len(subset) - 1)
+    
         
-        # 选nums[index]
-        subset.pop(len(subset) - 1)
-        self.dfs(nums, index + 1, subset, results)
